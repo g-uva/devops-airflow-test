f92d51ba95499369f91da890e23ff93c811e8654
Gonçalo <gteixei@uva.nl>
2025-02-20 08:36:55 +0100

    More experiments, trying to run the example file converted. Not successful, as I cannot download the image

--- dags/example_workday_timetable_copy.py
+++ dags/example_workday_timetable_copy.py
@@ -1,31 +1,63 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-from __future__ import annotations
+from airflow import DAG
+# from airflow.operators.docker_operator import DockerOperator
+# from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+# import json
 
-import pendulum
+# Define default arguments
+default_args = {
+    'owner': 'goncalo',
+    'start_date': datetime(2024, 2, 1),
+    'retries': 4
+}
 
-from airflow.example_dags.plugins.workday import AfterWorkdayTimetable
-from airflow.models.dag import DAG
-from airflow.operators.empty import EmptyOperator
+# # Define the DAG
+# dag = DAG(
+#     'converted_argo_workflow',
+#     default_args=default_args,
+#     schedule_interval=None
+# )
 
-with DAG(
-    dag_id="example_workday_timetable",
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    schedule=AfterWorkdayTimetable(),
-    tags=["example", "timetable"],
-):
-    EmptyOperator(task_id="run_this")
+# # Task 1: list-py-set
+# list_py_set = DockerOperator(
+#     task_id='list_py_set',
+#     image="ghcr.io/qcdis/naavre-cells-openlab/list-py-set-gabriel-pelouze-lifewatch-eu:307c95c",
+#     api_version='auto',
+#     auto_remove="success",
+#     command="source /venv/bin/activate; python /app/task.py --id 1ca0d5e",
+#     docker_url="unix://var/run/docker.sock",
+#     network_mode="bridge",
+#     # volumes=["/tmp/data"],
+#     dag=dag
+# )
+
+# # Task 2: splitter (Python function)
+# def splitter(**kwargs):
+#     ti = kwargs['ti']
+#     names_json = ti.xcom_pull(task_ids='list_py_set')
+#     names_list = json.loads(names_json)
+#     list_of_lists = [[name] for name in names_list]
+#     ti.xcom_push(key='splitter_output', value=json.dumps(list_of_lists))
+
+# splitter_task = PythonOperator(
+#     task_id='splitter',
+#     python_callable=splitter,
+#     provide_context=True,
+#     dag=dag
+# )
+
+# # Task 3: list-py-process
+# list_py_process = DockerOperator(
+#     task_id='list_py_process',
+#     image="ghcr.io/qcdis/naavre-cells-openlab/list-py-process-gabriel-pelouze-lifewatch-eu:d49895b",
+#     api_version='auto',
+#     auto_remove="success",
+#     command="source /venv/bin/activate; python /app/task.py --names '{{ ti.xcom_pull(task_ids='splitter', key='splitter_output') }}' --param_greeting_template 'Hello, {}!' --id 9eb8967",
+#     docker_url="unix://var/run/docker.sock",
+#     network_mode="bridge",
+#     # volumes=["/tmp/data"],
+#     dag=dag
+# )
+
+# # Define dependencies
+# list_py_set >> splitter_task >> list_py_process
